e1381f981ee60288c9e64d04f182b58e1c4ec49c
ssghost <ssprof0@gmail.com>
2023-08-29 23:03:26 +0800

    add gpio

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::asm;
 
 mod boot {
     use core::arch::global_asm;
@@ -12,7 +13,19 @@ mod boot {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    unsafe {
+        core::ptr::write_volatile(dst:0x3F20_0008 as *mut u32, src:1<<3);
+        loop {
+            core::ptr::write_volatile(dst:0x3F20_001C as *mut u32, src:1<<21);
+            for _ in 1..50000 {
+                asm!("nop")
+            }
+            core::ptr::write_volatile(dst:0x3F20_0028 as *mut u32, src:1<<21);
+            for _ in 1..50000 {
+                asm!("nop")
+            }
+        }
+    }
 }
 
 #[panic_handler]
