93197fa1af6f7ec5c21e76c0fae13696266710d9
ssghost <ssprof0@gmail.com>
2023-08-29 21:59:35 +0800

    add linker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod boot {
+    use core::arch::global_asm;
+    global_asm!(
+        ".section .text._start"
+    );
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic (_info: &PanicInfo) -> ! {
+    loop {}
 }
